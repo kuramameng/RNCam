b46a1790db0f6ded72b8750406c0abc06f075dc3
kuramameng <mengjs324@gmail.com>
2017-02-23 21:25:50 -0500

    call camera module and verify it works on nexus device

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,16 +1,29 @@
 'use strict';
 import React, { Component } from 'react';
 import { CardSection, Button } from './common';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Dimensions } from 'react-native';
+import Camera from 'react-native-camera';
 
 class MainView extends Component {
   render(){
-    const {previewWindow,toolbarMenu,filtersButton,snapButton,albumButton} = styles;
+    const {
+      previewWindow,
+      toolbarMenu,
+      filtersButton,
+      snapButton,
+      albumButton
+    } = styles;
 
     return (
       <View style={{flex:1}}>
         <CardSection style={previewWindow}>
-          <Text>test</Text>
+          <Camera
+            ref={(cam) => {
+              this.camera = cam;
+            }}
+            style={styles.preview}
+            aspect={Camera.constants.Aspect.fill}>
+          </Camera>
         </CardSection>
         <CardSection style={toolbarMenu}>
           <Button style={filtersButton}>Filters</Button>
@@ -24,8 +37,14 @@ class MainView extends Component {
 
 const styles = {
   previewWindow: {
-    flex: 9,
-    backgroundColor: 'skyblue'
+    flex: 9
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
   },
   toolbarMenu: {
     flex: 1,
