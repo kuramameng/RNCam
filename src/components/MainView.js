8b6177c47290b6aeaa31351d3747e1cd93f23c74
kuramameng <mengjs324@gmail.com>
2017-02-23 20:58:44 -0500

    set main view section flex ratio

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -5,13 +5,26 @@ import { StyleSheet, View, Text } from 'react-native';
 class MainView extends Component {
   render(){
     return (
-      <View>
-        <CardSection>
+      <View style={{flex:1}}>
+        <CardSection style={styles.previewWindow}>
           <Text>test</Text>
+        </CardSection>
+        <CardSection style={styles.toolbarMenu}>
         </CardSection>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  previewWindow: {
+    flex: 9,
+    backgroundColor: 'skyblue'
+  },
+  toolbarMenu: {
+    flex: 1,
+    backgroundColor: 'powderblue'
+  }
+});
 
 export default MainView;
