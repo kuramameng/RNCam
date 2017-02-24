5d356c9b50abef23f804539450203f4da50659d8
kuramameng <mengjs324@gmail.com>
2017-02-23 20:50:41 -0500

    create MainView.js and start constructing main view sections

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import MainView from './components/MainView';
 
 class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <MainView></MainView>
     );
   }
 }
---
+++ src/components/MainView.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { CardSection } from './common';
+import { StyleSheet, View, Text } from 'react-native';
+
+class MainView extends Component {
+  render(){
+    return (
+      <View>
+        <CardSection>
+          <Text>test</Text>
+        </CardSection>
+      </View>
+    )
+  }
+}
+
+export default MainView;
