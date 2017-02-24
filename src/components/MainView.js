78a7a0caf0debcbcdc829186e059cf033bc8ba32
kuramameng <mengjs324@gmail.com>
2017-02-23 21:11:49 -0500

    define 3 sections in the toolbar menu

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,30 +1,45 @@
 import React, { Component } from 'react';
-import { CardSection } from './common';
+import { CardSection, Button } from './common';
 import { StyleSheet, View, Text } from 'react-native';
 
 class MainView extends Component {
   render(){
+    const {previewWindow,toolbarMenu,filtersButton,snapButton,albumButton} = styles;
+
     return (
       <View style={{flex:1}}>
-        <CardSection style={styles.previewWindow}>
+        <CardSection style={previewWindow}>
           <Text>test</Text>
         </CardSection>
-        <CardSection style={styles.toolbarMenu}>
+        <CardSection style={toolbarMenu}>
+          <Button style={filtersButton}>Filters</Button>
+          <Button style={snapButton}>Snap</Button>
+          <Button style={albumButton}>Album</Button>
         </CardSection>
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   previewWindow: {
     flex: 9,
     backgroundColor: 'skyblue'
   },
   toolbarMenu: {
     flex: 1,
-    backgroundColor: 'powderblue'
+    backgroundColor: 'powderblue',
+    flexDirection: 'row'
+  },
+  filtersButton: {
+    flex: 3
+  },
+  snapButton: {
+    flex: 3
+  },
+  albumButton: {
+    flex: 3
   }
-});
+};
 
 export default MainView;
